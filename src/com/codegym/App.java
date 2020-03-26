ba5287ca4e1922741dadf31d0a8451ed30ab97d1
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-03-26 08:28:47 +0700

    init comit

---
+++ src/com/codegym/App.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+
+
+public class App {
+
+
+
+    public static void main(String[] args) {
+
+        Car car1 = new Car("Mazda 3","Skyactiv 3");
+
+        System.out.println(Car.numberOfCars);
+
+        Car car2 = new Car("Mazda 6","Skyactiv 6");
+
+        System.out.println(Car.numberOfCars);
+
+    }
+
+}
